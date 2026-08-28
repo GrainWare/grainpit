714d5ccb8546a37c4c4b2078a646803fc53626c3
electron271 <66094410+electron271@users.noreply.github.com>
2026-08-28 11:09:32 -0500

    remove compression thing

--- grainpit_webserver/src/main.rs
+++ grainpit_webserver/src/main.rs
@@ -55,7 +55,6 @@ async fn main() {
 async fn wildcard_handler(
     State(state): State<Arc<AppState>>,
     Path(path): Path<String>,
-    headers: HeaderMap,
 ) -> Response {
     if path.contains(".html") {
         handler(State(state)).await.into_response()
@@ -65,15 +64,6 @@ async fn wildcard_handler(
         headers.insert(header::CONTENT_TYPE, "image/jpeg".parse().unwrap());
         (headers, output).into_response()
     } else {
-        if let Some(header) = headers.get("Accept-Encoding")
-            && header.to_str().unwrap_or("").contains("br")
-        {
-            let mut headers = HeaderMap::new();
-            headers.insert(header::CONTENT_TYPE, "text/plain".parse().unwrap());
-            headers.insert(header::CONTENT_ENCODING, "br".parse().unwrap());
-            return (headers, include_bytes!("./g.txt.br")).into_response();
-        }
-
         let mut a = state.m.config_chain.generate(512);
         a = a.trim_start().to_owned();
         a.into_response()
