146cf7beecd3927e914ed6b4d6afc91d6bc6431f
electron271 <66094410+electron271@users.noreply.github.com>
2026-09-12 10:47:32 -0500

    pending fix by doclic

---
+++ grainpit.cgi/config.def.h
@@ -0,0 +1,8 @@
+/* Default configuration file for grainpit.cgi */
+
+/* make sure they all with a slash */
+static const char *grainpit_extraurls[] = {
+    "https://example.com/",
+};
+/* range from 0 to 100, 0 means never, 100 means always */
+static int grainpit_extraurls_chance = 5;
