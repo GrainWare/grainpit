528ce9ba7625c689695e9a6e90ca7f598876b085
electron271 <66094410+electron271@users.noreply.github.com>
2026-09-12 09:58:15 -0500

    remove zune-imageprocs until further notice

--- grainpit/src/image.rs
+++ grainpit/src/image.rs
@@ -1,7 +1,7 @@
 use rand::RngExt;
 use zune_core::options::EncoderOptions;
 use zune_image::{codecs::jpeg::JpegEncoder, traits::OperationsTrait};
-use zune_imageprocs::{invert::Invert, sobel::Sobel};
+//use zune_imageprocs::{invert::Invert, sobel::Sobel}; // disabled until zune gets their shit together and updates this package
 
 pub fn gen_image() -> Vec<u8> {
     let img_x = 88;
@@ -12,19 +12,20 @@ pub fn gen_image() -> Vec<u8> {
     let mut pixels = [0; (88 * 31) * 3];
     rand::rng().fill(&mut pixels);
 
-    let mut image = zune_image::image::Image::from_u8(
+    //let mut image = zune_image::image::Image::from_u8(
+    let image = zune_image::image::Image::from_u8(
         &pixels,
         img_x,
         img_y,
         zune_core::colorspace::ColorSpace::RGB,
     );
 
-    let sobel = Sobel::new();
-    sobel.execute(&mut image).unwrap();
-    if rand::random_bool(0.5) {
-        let invert = Invert::new();
-        invert.execute(&mut image).unwrap();
-    }
+    //let sobel = Sobel::new();
+    //sobel.execute(&mut image).unwrap();
+    //if rand::random_bool(0.5) {
+    //    let invert = Invert::new();
+    //    invert.execute(&mut image).unwrap();
+    //}
 
     let mut output = vec![];
     image.write_with_encoder(encoder, &mut output).unwrap();
